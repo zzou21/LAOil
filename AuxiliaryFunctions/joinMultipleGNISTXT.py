6894fbbafcd501d77fe4b42c15ec49c164772fd0
Zhihui (Jerry) Zou <zz341@duke.edu>
2024-11-25 12:42:43 -0500

    tesT

---
+++ AuxiliaryFunctions/joinMultipleGNISTXT.py
@@ -0,0 +1,6 @@
+'''
+This program combines the Domestic names data from GNIS that are stored in 50 different TXT files into one TXT
+Data gained from: https://prd-tnm.s3.amazonaws.com/index.html?prefix=StagedProducts/GeographicNames/DomesticNames/
+'''
+import pandas as pd
+import os
