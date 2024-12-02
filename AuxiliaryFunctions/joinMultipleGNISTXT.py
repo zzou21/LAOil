ed762779a871cf5762671c7e18c1a129089a9d1d
Jerry Zou <zhihui.zou@duke.edu>
2024-12-02 12:16:16 -0500

    added stuff.

--- AuxiliaryFunctions/joinMultipleGNISTXT.py
+++ AuxiliaryFunctions/joinMultipleGNISTXT.py
@@ -6,25 +6,29 @@ import pandas as pd
 import os
 
 class manipulateTXT:
-    def __init__(self, multipleTXTJoinFolderPath, storageJoinedOneTXTDestination):
+    def __init__(self, multipleTXTJoinFolderPath, storageJoinedOneTXTDestinationCSV):
         self.multipleTXTJoinFolderPath = multipleTXTJoinFolderPath
-        self.storageJoinedOneTXTDestination = storageJoinedOneTXTDestination
+        self.storageJoinedOneTXTDestinationCSV = storageJoinedOneTXTDestinationCSV
     
     # This function joins multiple TXTs that are in the format of pipe-delimited and join them together. This function only works when all TXT files being joined follow the same pipe-delimited headers, data types, and format.
     def joinMultipleTXT(self):
-        takenOneHeader = True
         filePathStorageListToIterate = []
         for txtFile in os.listdir(self.multipleTXTJoinFolderPath):
             if txtFile.endswith(".txt"):
                 fullOneFilePath = os.path.join(self.multipleTXTJoinFolderPath, txtFile)
                 filePathStorageListToIterate.append(fullOneFilePath)
         filePathStorageListToIterate = sorted(filePathStorageListToIterate, key = lambda x: x[-6:-4])
-        
-        
+        combinedDataFramePrepList = []
+        for txtFilePath in filePathStorageListToIterate:
+            temporaryDF = pd.read_csv(txtFilePath, sep = "|", dtype = str)
+            combinedDataFramePrepList.append(temporaryDF)
+        finalCombinedDataFrame = pd.concat(combinedDataFramePrepList, ignore_index = True)
+        finalCombinedDataFrame.to_csv(self.storageJoinedOneTXTDestinationCSV, index = False)
+        print("Finished combining CSVs.")
 
 
 if __name__ == "__main__":
     multipleTXTJoinFolderPath = "/Users/Jerry/Desktop/DH proj-reading/LAOilNewspaper/GNISDomesticNamesAllStates/Text"
-    storageJoinedOneTXTDestination = "/Users/Jerry/Desktop/DH proj-reading/LAOilNewspaper/GNISDomesticNamesAllStates"
-    manipulateTXTArchitecture = manipulateTXT(multipleTXTJoinFolderPath, storageJoinedOneTXTDestination)
+    storageJoinedOneTXTDestinationCSV = "/Users/Jerry/Desktop/DH proj-reading/LAOilNewspaper/GNISDomesticNamesAllStates/combinedAllStatesContent.csv"
+    manipulateTXTArchitecture = manipulateTXT(multipleTXTJoinFolderPath, storageJoinedOneTXTDestinationCSV)
     manipulateTXTArchitecture.joinMultipleTXT()
