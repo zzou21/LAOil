fd91426c84afe724da05c44930ad65f142899fa8
zzou21 <jerryzouzhihui@gmail.com>
2024-12-04 13:16:57 -0500

    workd on combining two dataframes

--- AuxiliaryFunctions/extractLocationDataFromName.py
+++ AuxiliaryFunctions/extractLocationDataFromName.py
@@ -9,7 +9,30 @@ class findLocationData:
         self.outputCSVLocation = outputCSVLocation
     
     def readGNISCSV(self):
+        GNISData = pd.read_csv(self.GNISCSV, sep = ",", dtype = str)
+        print(f"Column titles: {GNISData.columns}")
+        GNISDataFilteredCivil = GNISData[ #filter original raw dataframe
+            GNISData["feature_name"].str.contains("city", case = False, na = False) |
+            GNISData["feature_name"].str.contains("town", case = False, na = False) |
+            GNISData["feature_class"].str.contains("civil", case = False, na = False)
+        ]
+        print(GNISDataFilteredCivil.head())
+        print(len(GNISDataFilteredCivil.index))
+        return GNISDataFilteredCivil
+    
+    def readNewspaperCSV(self):
+        newspaperDataRaw = pd.read_csv(self.newspaperCSV, sep = ",", dtype = str)
+        newspaperDataRaw = newspaperDataRaw.drop("Unnamed: 0", axis = 1)
+        print(f"Column titles: {newspaperDataRaw.columns}")
+        print(newspaperDataRaw.head())
+        
+
 
 
 if __name__ == "__main__":
-    newspaperCSV = ""
+    newspaperCSV = "updatedCSVNewspaper.csv"
+    GNISCSV = "C:/Users/zz341/Desktop/combinedAllStatesContent.csv"
+    outputCSVLocation = ""
+    locationDataFindMachine = findLocationData(newspaperCSV, GNISCSV, outputCSVLocation)
+    # locationDataFindMachine.readGNISCSV()
+    locationDataFindMachine.readNewspaperCSV()
